f6ade56e0e764169530065cce17bd95af9cb2b39
AhmadNaeemQA <ahmadzeya.naeem@academytrainee.com>
2020-09-03 15:57:04 +0100

    updated

--- Client/client/src/Contact.js
+++ Client/client/src/Contact.js
@@ -4,8 +4,12 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar.js'
 import Footer from './Footer.js'
+import nodemailer from 'nodemailer';
+import axios from 'axios';
 
 export default class Contact extends Component{
+
+
     render(){
         return(
             <div>
--- Client/client/src/Navbar.js
+++ Client/client/src/Navbar.js
@@ -8,22 +8,22 @@ export default class Navbar extends Component{
 
     render(){
         return (
-            <nav class="navbar navbar-expand-lg navbar" style={{backgroundColor: "black"}} >
-                <a class="navbar-brand" href="#" style={{color: "white", fontSize: "30px", paddingRight: "100px"}}><Link to="/"><img src="../media/logo.png" style={{width: "230px", height: "100px"}}/></Link></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar" style={{backgroundColor: "black"}} >
+                <a className="navbar-brand" href="#" style={{color: "white", fontSize: "30px", paddingRight: "100px"}}><Link to="/"><img src="../media/logo.png" style={{width: "230px", height: "100px"}}/></Link></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent" style={{color: "white"}}>
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#" style={{color: "white", fontSize: "20px" , paddingRight: "30px"}}>Info<span class="sr-only">(current)</span></a>
+ 
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{color: "white"}}>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#" style={{color: "white", fontSize: "20px" , paddingRight: "30px"}}>Info<span className="sr-only">(current)</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style={{color: "white", fontSize: "20px" , paddingRight: "30px"}}><Link to = {`/contact`}>Contact Us</Link></a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" style={{color: "white", fontSize: "20px" , paddingRight: "30px"}}><Link to="/contact">Contact Us</Link></a>
                         </li>
                     </ul>
-                    <Search/>
+                    <Search></Search>
                 </div>
             </nav>
         )
